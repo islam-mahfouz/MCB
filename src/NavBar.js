4ed81c60c5e29d9b4176cd24b24e1b602aee1650
islam <islamheshamashraf@gmail.com>
2016-10-06 07:17:01 +0300

    added main layout

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,18 +8,18 @@ export default class NavBar extends React.Component {
 		<nav id="nav-bar">
 		    <div class="nav-wrapper">
 		      <a href="JavaScript:" class="brand-logo"><img src="/img/logo/feedme_logo_white.png" />feed.me</a>
-		      <a href="JavaScript:" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+		      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 		      <ul  class="right hide-on-med-and-down">
 		        <li><a href="JavaScript:">OVERVIEW</a></li>
 		        <li><a href="JavaScript:">TODAY'S MEAL</a></li>
 		        <li><a href="JavaScript:">MORE</a></li>
-		        <li><a class="waves-effect waves-light btn">ORDER TODAY'S MEAL</a></li>
+		        <li><a class="waves-effect waves-light btn red-button">ORDER TODAY'S MEAL</a></li>
 		      </ul>
 		      <ul  class="side-nav" id="mobile-demo">
 		        <li><a href="JavaScript:">OVERVIEW</a></li>
 		        <li><a href="JavaScript:">TODAY'S MEAL</a></li>
 		        <li><a href="JavaScript:">MORE</a></li>
-		        <li><a class="waves-effect waves-light btn">ORDER TODAY'S MEAL</a></li>
+		        <li><a class="waves-effect waves-light btn red-button">ORDER TODAY'S MEAL</a></li>
 		      </ul>
 		    </div>
 	  	</nav>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
-import NavBar from './NavBar';
+import Layout from './Layout';
  
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    React.createElement(NavBar),
+    React.createElement(Layout),
     document.getElementById('app')
   );
 });
